0b78f12b89d82d45c89ae584c216ccb5ebe96905
Irving <irving.colli@mx.lms-la.com>
2023-07-22 18:47:36 -0600

    Initial Commit

---
+++ ejemplo/ejemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejemploApp.swift
+//  ejemplo
+//
+//  Created by Irving on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ejemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
